47c39fc8af7bd1150ceaab13d2dd1784a9f7e893
Rory Caston <rory@caston.co.uk>
2024-05-18 15:31:21 +0100

    added dependency; implemented command pattern; added .gitignore

---
+++ fantasy-tournament-bot/src/main/java/com/glaeriasmite/fantasy/bot/handlers/Handler.java
@@ -0,0 +1,13 @@
+package com.glaeriasmite.fantasy.bot.handlers;
+
+import com.glaeriasmite.fantasy.bot.commands.Command;
+
+public class Handler {
+
+    public void execute(Command command) {
+
+        command.execute();
+
+    }
+
+}

25d5b19f459743b4d4e33a6d6f808e922942cb37
Rory Caston <rory@caston.co.uk>
2024-05-18 18:33:48 +0100

    bot reads token; connects using token; responds to message

--- fantasy-tournament-bot/src/main/java/com/glaeriasmite/fantasy/bot/App.java
+++ fantasy-tournament-bot/src/main/java/com/glaeriasmite/fantasy/bot/App.java
@@ -1,18 +1,33 @@
 package com.glaeriasmite.fantasy.bot;
 
+import java.io.File;
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
-
-/**
- * Hello world!
- *
- */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws IOException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(new File("../config.json"));
+        String token = jsonNode.get("discord_token").asText();
+
+        JDABuilder build = JDABuilder.createDefault(token);
+        build.setActivity(Activity.watching("playing Donkey Kong Country"));
+        build.setStatus(OnlineStatus.ONLINE);
+
+        JDA jda = build.enableIntents(GatewayIntent.MESSAGE_CONTENT)
+            .build();
+
+        jda.addEventListener(new MessageListener()); 
+
     }
+
 }

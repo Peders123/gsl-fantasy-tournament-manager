0b8ce32ad1ef611d66a90657f84bf41ffbbb9ca1
Rory Caston <rory@caston.co.uk>
2024-05-25 15:09:21 +0100

    added tournament model

--- gsl_fantasy_tournament/tournament/models.py
+++ gsl_fantasy_tournament/tournament/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
+class Tournament(models.Model):
+
+    tournament_id = models.IntegerField(primary_key=True)
+    date = models.DateField()
+    title = models.CharField(max_length=64, default="Glaeria Smite League")
+    description = models.CharField(max_length=256, default="None")
+
 
 class User(models.Model):
 
     user_id = models.IntegerField(primary_key=True)
     discord_name = models.CharField(max_length=32)
     smite_name = models.CharField(max_length=32)
+    tournament_id = models.ForeignKey(Tournament, on_delete=models.CASCADE, default=0)
 
 
 class Captain(models.Model):

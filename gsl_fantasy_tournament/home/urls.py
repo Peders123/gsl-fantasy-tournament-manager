be4de48e23b229264e574974c6876814a2e0825b
revilokid <72077134+revilokid@users.noreply.github.com>
2024-05-28 11:22:28 +0100

    Add files via upload

--- gsl_fantasy_tournament/home/urls.py
+++ gsl_fantasy_tournament/home/urls.py
@@ -1,7 +1,16 @@
-from django.urls import path
+
+from django.urls import include, path
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('Users/', views.User_list),
+    path('Users/<int:User_id>', views.User_detail),
+    path('Tournaments/', views.Tournament_list),
+    path('Tournaments/<int:tournament_id>', views.Tournament_detail),
+    path('Captains/', views.Captain_list),
+    path('Captain/<int:captain_id>', views.Captain_detail),
+    path('Players/', views.Player_list),
+    path('Players/<int:player_id>', views.Player_detail)
 ]

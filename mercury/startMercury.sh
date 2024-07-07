ea371687e197a03da63412a9c3e8115bc5061df6
Rory Caston <rory@caston.co.uk>
2024-07-07 20:18:12 +0100

    added auth to mercury

--- mercury/startMercury.sh
+++ mercury/startMercury.sh
@@ -11,6 +11,14 @@ python3 manage.py migrate --database=dev
 if [ $BUILD_TYPE = "dev" ]; then
     echo "RUNNING DEV"
     python3 manage.py populate_database
+    expect -c "
+    spawn python manage.py createsuperuser --database=dev --username=Peders --email=admin@example.com
+    expect \"Password:\"
+    send \"Pa55we1rd\r\"
+    expect \"Password (again):\"
+    send \"Pa55we1rd\r\"
+    expect eof
+    "
 fi
 
 python3 manage.py runserver 0.0.0.0:8001
--- mercury/talaria/models.py
+++ mercury/talaria/models.py
@@ -4,6 +4,18 @@ from datetime import datetime
 
 from django.db import models
 
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+
+    if created:
+        Token.objects.using(os.environ['BUILD_TYPE']).create(user=instance)
+
 
 class Tournament(models.Model):
     """Model representing an individual tournament.

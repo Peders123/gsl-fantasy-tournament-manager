9d77c730599ec0b12b2396123e963146768fc313
Rory Caston <rory@caston.co.uk>
2024-07-12 18:19:31 +0100

    renamed website to 'terra'; fixed a small build issue

---
+++ terra/home/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from tournament.models import Tournament
+
+
+def index(request):
+
+    tournaments = Tournament.objects.all()
+
+    context = {
+        "tournaments": tournaments
+    }
+
+    return render(request, 'home/home.html', context)
--- terra/terra/asgi.py
+++ terra/terra/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for gsl_fantasy_tournament project.
+ASGI config for terra project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gsl_fantasy_tournament.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'terra.settings')
 
 application = get_asgi_application()

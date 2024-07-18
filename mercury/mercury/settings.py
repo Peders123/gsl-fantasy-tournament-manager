c6083978eb7ec0c6287c34fbb2762d452a64af44
revilokid <72077134+revilokid@users.noreply.github.com>
2024-07-18 18:57:00 +0100

    Update settings.py

--- mercury/mercury/settings.py
+++ mercury/mercury/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&5w+v%pk339$m6+n)(pujq8@zl#$&o*#7h_14$k#eb6u+1azsf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

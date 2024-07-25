7c8bcaa478811ada6144a13090aca202c0bfd206
Rory Caston <43795065+Peders123@users.noreply.github.com>
2024-07-25 22:07:47 +0100

    fixed syntax error

--- mercury/mercury/settings.py
+++ mercury/mercury/settings.py
@@ -33,6 +33,7 @@ DATABASE_SETUPS = {
         'default': {
             'NAME': BASE_DIR / 'db.sqlite3',
             'ENGINE': 'django.db.backends.sqlite3',
+        }
     },
     "ops": {
         "default": {

ba281d4ce73374c6c7d43cc5289208a7a80ffbd6
Rory Caston <rory@caston.co.uk>
2024-07-30 21:29:41 +0100

    made asychronous

--- terra/auction/consumers.py
+++ terra/auction/consumers.py
@@ -1,44 +1,38 @@
 import json
 
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 
-class AuctionConsumer(WebsocketConsumer):
+class AuctionConsumer(AsyncWebsocketConsumer):
 
-    def connect(self):
+    async def connect(self):
 
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = f"chat_{self.room_name}"
 
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name, self.channel_name
-        )
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        await self.accept()
 
-        self.accept()
-
-    def disconnect(self, close_code):
+    async def disconnect(self, code):
         
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name, self.channel_name
-        )
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
-    def receive(self, text_data):
-
+    async def receive(self, text_data):
+        
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.room_group_name, {
                 'type': 'chat.message',
                 'message': message
             }
         )
 
-    def chat_message(self, event):
+    async def chat_message(self, event):
 
         message = event['message']
 
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'message': message
         }))

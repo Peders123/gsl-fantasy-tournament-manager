574b9702291be49730a8ca53f61c12450cfc13d2
Rory Caston <rory@caston.co.uk>
2024-08-03 11:25:54 +0100

    linting

--- mercury/mercury/settings.py
+++ mercury/mercury/settings.py
@@ -34,7 +34,7 @@ CSRF_TRUSTED_ORIGINS = ['https://mercury.tanukismiteleague.com']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
- 
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = CREDENTIALS['tokens']['secret-key']['mercury']
 

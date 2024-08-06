15a553392b49ff1b5c020191ee0c95953054e9b0
Rory Caston <rory@caston.co.uk>
2024-08-06 13:47:10 +0100

    no longer allows non-captains into the auction room

--- terra/auction/urls.py
+++ terra/auction/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.auction, name='auction'),
     path('<str:room_name>/', views.room, name='room'),
 ]

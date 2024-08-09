72a58342849a10845de314d8b90c2b99832017f4
Rory Caston <rory@caston.co.uk>
2024-08-09 11:47:07 +0100

    small startup change

--- terra/startTerra.sh
+++ terra/startTerra.sh
@@ -6,7 +6,7 @@ python3 manage.py migrate auction
 
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py collectstatic
+python3 manage.py collectstatic --noinput
 
 export DJANGO_SETTINGS_MODULE=terra.settings
 daphne -b 0.0.0.0 -p 8000 terra.asgi:application

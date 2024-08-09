ca9fc6d234d9e610f8b28118c13f7f9168d500ff
revilokid <72077134+revilokid@users.noreply.github.com>
2024-08-09 16:31:46 +0100

    Update admin.py

--- terra/home/admin.py
+++ terra/home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Suggestion
+
+class SuggestionAdmin(admin.ModelAdmin):
+    
+    list_display = ("suggestion_id","player_name","discord_nametag","suggested_value","tournament_id")
+    
+admin.site.register(Suggestion, SuggestionAdmin)

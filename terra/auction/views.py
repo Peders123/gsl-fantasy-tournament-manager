14bda4bc3b833c676c2b17178d3557ac7feb7eb1
Rory Caston <rory@caston.co.uk>
2024-08-09 10:35:36 +0100

    auction is closed until tournament start

--- terra/auction/views.py
+++ terra/auction/views.py
@@ -27,6 +27,13 @@ def room(request, room_name):
         url = reverse('auction')
         error = "User is not registered as a captain."
         return redirect(f"{url}?errno=401&error={error}")
+    
+    tournament = Tournament.objects.get(tournament_id=1)
+
+    if tournament.is_past():
+        url = reverse('auction')
+        error = "This tournament hasn't started yet."
+        return redirect(f"{url}?errno=401&error={error}")
 
     bidders = Bidder.objects.filter(
         tournament_id=1,

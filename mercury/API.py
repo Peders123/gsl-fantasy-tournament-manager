71443f9e486802bcee923d00a57afb5fbb12f679
Revilokid <Oliver.m.pohl05@gmail.com>
2024-09-01 07:32:41 +0100

    local server is set. not sure if sql.exe is needed

--- mercury/API.py
+++ mercury/API.py
@@ -35,6 +35,7 @@ def create_User(user: User, db:Session = Depends(get_db)):
     user_model = models.Users()
     user_model.id = user.id
     user_model.discord_name = user.discord_name
+    
 
     db.add(user_model)
     db.commit()
@@ -42,29 +43,37 @@ def create_User(user: User, db:Session = Depends(get_db)):
     return user
 
 @app.put("/{user_id}")
-def update_User(user_id: str, user: User):
-    counter = 0
+def update_User(user_id: str, user: User, db: Session = Depends(get_db)):
 
-    for x in USERS:
-        counter = counter + 1
-        if x.id == user_id:
-            USERS[counter - 1] = user
-            return USERS[counter - 1]
-    raise HTTPException(
-        status_code=404,
-        detail=f"ID {user_id} : Does not exist :(ↄc)"
-    )
+    user_model = db.query(models.Users).filter(models.Users.id == user_id).first()
+
+    if user_model is None:
+        raise HTTPException(
+            status_code=404,
+            detail=f"ID {user_id} : Does not exist :(ↄc)"
+        )
+
+    user_model.id = user.id
+    user_model.discord_name = user.discord_name
+
+    db.add(user_model)
+    db.commit()
+
+    return user
+
 
 @app.delete("/{user_id}")
-def delete_user(user_id: str):
-        counter = 0
+def delete_user(user_id: str, db: Session = Depends(get_db)):
+        
+    user_model =  db.query(models.Users).filter(models.Users.id == user_id).first()
 
-        for x in USERS:
-            counter = counter + 1
-            if x.id == user_id:
-                del USERS[counter - 1]
-                return f"ID: {user_id} deleted"
+
+    if user_model is None:
         raise HTTPException(
-        status_code=404,
-        detail=f"ID {user_id} : Does not exist :(ↄc)"
-    )
+            status_code=404,
+            detail=f"ID {user_id} : Does not exist :(ↄc)"
+        )
+    
+    db.query(models.Users).filter(models.Users.id == user_id).delete()
+
+    db.commit()

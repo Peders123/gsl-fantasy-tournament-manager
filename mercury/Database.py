30d2895c851596ef8355bf246e93b14d5c45316f
Revilokid <Oliver.m.pohl05@gmail.com>
2024-09-02 08:08:39 +0100

    lint changes in Database.py file

--- mercury/Database.py
+++ mercury/Database.py
@@ -5,12 +5,11 @@ from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./TSL.db"
 
-engine= create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread":False}
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine )
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-

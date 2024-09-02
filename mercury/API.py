a5d5d24e7103d8cb032ef0c5c64f54d83b9c1f65
Revilokid <Oliver.m.pohl05@gmail.com>
2024-09-02 07:24:54 +0100

    got all the old models in now

--- mercury/API.py
+++ mercury/API.py
@@ -23,11 +23,35 @@ class User(BaseModel):
     id: str = Field(max_length=64)
     discord_name: str = Field(max_length=32)
 
+class Tournament(BaseModel):
+    #date
+    title: str = Field(min_length=1, max_length=64)
+    description: str = Field(max_length=256)
+
+class Captain(BaseModel):
+    tournament_id: int = Field()
+    user_id: str = Field()
+    smite_name: str = Field()
+    team_name: str = Field()
+    reason: str = Field()
+    captain_budget: int = Field()
+
+class Player(BaseModel):
+    tournament_id: int = Field()
+    user_id: str = Field()
+    captain_id: str = Field()
+    smite_name: str = Field ()
+    role_1: str = Field()
+    role_2: str = Field()
+    smite_guru: str = Field()
+    estimated_value: int = Field()
+
+
 @app.get("/Users")
 def read_api(db: Session = Depends(get_db)):
     return db.query(models.Users).all()
 
-USERS = []
+
 
 @app.post("/Users")
 def create_User(user: User, db:Session = Depends(get_db)):
@@ -75,5 +99,193 @@ def delete_user(user_id: str, db: Session = Depends(get_db)):
         )
     
     db.query(models.Users).filter(models.Users.id == user_id).delete()
+
+    db.commit()
+
+@app.get("/Tournaments")
+def read_api(db: Session = Depends(get_db)):
+    return db.query(models.Tournaments).all()
+
+
+
+@app.post("/Tournaments")
+def create_Tournament(tournament: Tournament, db:Session = Depends(get_db)):
+    
+    tournament_model = models.Tournaments()
+    tournament_model.title = tournament.title
+    tournament_model.description = tournament.description
+
+    
+
+    db.add(tournament_model)
+    db.commit()
+
+    return tournament
+
+@app.put("/Tournaments/{tournament_id}")
+def update_Tournament(tournament_id: int, tournament: Tournament, db: Session = Depends(get_db)):
+
+    tournament_model = db.query(models.Tournaments).filter(models.Tournaments.id == tournament_id).first()
+
+    if tournament_model is None:
+        raise HTTPException(
+            status_code=404,
+            detail=f"ID {tournament_id} : Does not exist :(ↄc)"
+        )
+
+    tournament_model.title = tournament.title
+    tournament_model.description = tournament.description
+
+    db.add(tournament_model)
+    db.commit()
+
+    return tournament
+
+
+@app.delete("/Tournaments/{tournament_id}")
+def delete_tournament(tournament_id: int, db: Session = Depends(get_db)):
+        
+    tournament_model =  db.query(models.Tournaments).filter(models.Tournaments.id == tournament_id).first()
+
+
+    if tournament_model is None:
+        raise HTTPException(
+            status_code=404,
+            detail=f"ID {tournament_id} : Does not exist :(ↄc)"
+        )
+    
+    db.query(models.Tournaments).filter(models.Tournaments.id == tournament_id).delete()
+
+    db.commit()
+
+@app.get("/Captains")
+def read_api(db: Session = Depends(get_db)):
+    return db.query(models.Captains).all()
+
+
+
+@app.post("/Captains")
+def create_Captain(captain: Captain, db:Session = Depends(get_db)):
+    
+    captain_model = models.Captains()
+    captain_model.tournament_id = captain.tournament_id
+    captain_model.user_id = captain.user_id
+    captain_model.smite_name = captain.smite_name
+    captain_model.team_name = captain.team_name
+    captain_model.reason = captain.reason
+    captain_model.captain_budget = captain.captain_budget
+
+    
+
+    db.add(captain_model)
+    db.commit()
+
+    return captain
+
+@app.put("/Captains/{captain_id}")
+def update_Captain(captain_id: int, captain: Captain, db: Session = Depends(get_db)):
+
+    captain_model = db.query(models.Captains).filter(models.Captains.id == captain_id).first()
+
+    if captain_model is None:
+        raise HTTPException(
+            status_code=404,
+            detail=f"ID {captain_id} : Does not exist :(ↄc)"
+        )
+
+    captain_model.tournament_id = captain.tournament_id
+    captain_model.user_id = captain.user_id
+    captain_model.smite_name = captain.smite_name
+    captain_model.team_name = captain.team_name
+    captain_model.reason = captain.reason
+    captain_model.captain_budget = captain.captain_budget
+
+
+    db.add(captain_model)
+    db.commit()
+
+    return captain
+
+
+@app.delete("/Captains/{captain_id}")
+def delete_captain(captain_id: int, db: Session = Depends(get_db)):
+        
+    captain_model =  db.query(models.Captains).filter(models.Captains.id == captain_id).first()
+
+
+    if captain_model is None:
+        raise HTTPException(
+            status_code=404,
+            detail=f"ID {captain_id} : Does not exist :(ↄc)"
+        )
+    
+    db.query(models.Captains).filter(models.Captains.id == captain_id).delete()
+
+    db.commit()
+
+@app.get("/Players")
+def read_api(db: Session = Depends(get_db)):
+    return db.query(models.Players).all()
+
+
+
+@app.post("/Players")
+def create_Player(player: Player, db:Session = Depends(get_db)):
+    
+    player_model = models.Players()
+    player_model.tournament_id = player.tournament_id
+    player_model.user_id = player.user_id
+    player_model.smite_name = player.smite_name
+    player_model.role_1 = player.role_1
+    player_model.role_2 = player.role_2
+    player_model.smite_guru = player.smite_guru
+    player_model.estimated_value = player.estimated_value
+
+    
+
+    db.add(player_model)
+    db.commit()
+
+    return player
+
+@app.put("/Players/{player_id}")
+def update_Player(player_id: int, player: Player, db: Session = Depends(get_db)):
+
+    player_model = db.query(models.Players).filter(models.Players.id == player_id).first()
+
+    if player_model is None:
+        raise HTTPException(
+            status_code=404,
+            detail=f"ID {player_id} : Does not exist :(ↄc)"
+        )
+
+    player_model.tournament_id = player.tournament_id
+    player_model.user_id = player.user_id
+    player_model.smite_name = player.smite_name
+    player_model.role_1 = player.role_1
+    player_model.role_2 = player.role_2
+    player_model.smite_guru = player.smite_guru
+    player_model.estimated_value = player.estimated_value
+
+
+    db.add(player_model)
+    db.commit()
+
+    return player
+
+
+@app.delete("/Players/{player_id}")
+def delete_player(player_id: int, db: Session = Depends(get_db)):
+        
+    player_model =  db.query(models.Players).filter(models.Players.id == player_id).first()
+
+
+    if player_model is None:
+        raise HTTPException(
+            status_code=404,
+            detail=f"ID {player_id} : Does not exist :(ↄc)"
+        )
+    
+    db.query(models.Players).filter(models.Players.id == player_id).delete()
 
     db.commit()
--- mercury/models.py
+++ mercury/models.py
@@ -1,8 +1,79 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from Database import Base
 
 class Users(Base):
-    __tablename__ = "users"
+    __tablename__ = "User"
+
+    """Model representing a single user, mapping 1-to-1 with a discord account.
+
+    Attributes:
+        user_id (CharField): Primary key. Same as the user's discord id.
+        discord_name (CharField): Signed up user's discord name.
+    """
 
     id = Column(String, primary_key=True)
     discord_name = Column(String)
+
+class Tournaments(Base):
+    __tablename__ = "Tournament"
+
+    """Model representing an individual tournament.
+
+    Attributes:
+        tournament_id (IntegerField): Primary key for a tournament record.
+        datetime (DateTimeField): The time the tournament takes place.
+        title (CharField): The user-facing name of the tournament.
+        description (CharField): Brief description of what the touranment is.
+    """
+    id = Column(Integer, primary_key=True, index=True)
+    # datetime = models.DateTimeField(default=datetime(year=2001, month=11, day=5, hour=0, minute=0, second=0))
+    title = Column(String)
+    description = Column(String)
+
+class Captains(Base):
+    __tablename__ = "Captain"
+    """Model representing a captain for the specific tournament.
+
+    Attributes:
+        captain_id (IntegerField): Primary key. Is assigned automatically.
+        tournament_id (ForeignKey): Relation to the 'Tournaments' model.
+        user_id (ForeignKey): Relation to the 'User' model.
+        smite_name (CharField): Sign up user's smite ign.
+        team_name (CharField): The name of the captain's team.
+        reason (CharField): Justification for why the person should be a captain.
+        captain_budget (IntegerField): Budget the captain will begin the tournament with.
+    """
+    id = Column(Integer, primary_key=True, index=True)
+    tournament_id = Column(Integer, ForeignKey("Tournament.id",ondelete="CASCADE"), nullable=False)
+    user_id = Column(String, ForeignKey("User.id",ondelete="CASCADE"), nullable=False)
+    smite_name = Column(String)
+    team_name = Column(String)
+    reason = Column(String)
+    captain_budget = Column(Integer)
+
+class Players(Base):
+    __tablename__ = "Player"
+
+    """Model representing a standard player in a tournament.
+
+    Attributes:
+        player_id (AutoField): Primary key. Is assigned automatically.
+        tournament_id (ForeignKey): Relation to the 'Tournament' model.
+        user_id (ForeignKey): Relation to the 'User' model.
+        captain_id (ForeignKey): Relation to the 'Captain' model. Not assiged at creation, added later.
+        smite_name (CharField): Sign up user's smite ign.
+        role_1 (CharField): Primary role the player wants to be.
+        role_2 (CharField): Secondary role the player wants to be.
+        smite_guru (CharField): Link to the player's smite guru profile. Optional.
+        estimated_value (IntegerField): How much we estimate the player is worth.
+    """
+    id = Column(Integer, primary_key=True, index=True)
+    tournament_id = Column(Integer, ForeignKey("Tournament.id",ondelete="CASCADE"), nullable=False)
+    user_id = Column(String, ForeignKey("User.id",ondelete="CASCADE"), nullable=False)
+    captain_id = Column(Integer, ForeignKey("Captain.id",ondelete="CASCADE"))
+    smite_name = Column(String)
+    role_1 = Column(String)
+    role_2 = Column(String)
+    smite_guru = Column(String)
+    estimated_value = Column(Integer)
+

ed02cbd49e3d7642038412baf5afdac27b69da73
revilokid <72077134+revilokid@users.noreply.github.com>
2024-09-02 20:33:50 +0100

    Update startMercury.sh

--- mercury/startMercury.sh
+++ mercury/startMercury.sh
@@ -3,12 +3,5 @@
 cd /data/mercury
 
 
-echo "Migrating general"
-echo "migrations are yet to be added"
-
-echo "Finished Migrations"
-
-
-echo "Running Server"
 
 python3 -m uvicorn api:app --host 0.0.0.0 --port 80

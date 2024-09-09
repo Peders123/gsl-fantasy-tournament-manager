e0771c0c383dafdf73a504375b54f723dce9cee5
Rory Caston <rory@caston.co.uk>
2024-09-09 19:28:04 +0100

    close database session

--- mercury/main.py
+++ mercury/main.py
@@ -1,6 +1,16 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
+from dependencies import SESSIONMANAGER
 from routers.captains import captain_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+
+    yield
+    await SESSIONMANAGER.close()
 
 
 app = FastAPI()
